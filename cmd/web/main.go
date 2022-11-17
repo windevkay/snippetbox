a72bdb2a638d33b7d3a2d18bb096a1024688f438
Kola Adebayo <kolaadebayo@Kolas-MacBook-Pro.local>
2022-11-16 21:45:23 -0700

    refactoring app routes

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -27,18 +27,11 @@ func main() {
 		infoLog: infoLog,
 	}
 
-	mux := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
 	// override http defaults e.g. ErrorLog
 	srv := &http.Server{
 		Addr: *addr,
 		ErrorLog: errorLog,
-		Handler: mux,
+		Handler: app.routes(),
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
