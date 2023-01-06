9d7dde79e1841f56fab182bdc0fd8153d7179c16
Kola Adebayo <kolaadebayo@Kolas-MacBook-Pro.local>
2023-01-06 11:34:05 -0700

    panic recovery and composable middleware

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
+
+// Set OWASP Security Headers on responses
 func secureHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Security-Policy", "default-src 'self'; style-src 'self' fonts.googleapis.com; font-src fonts.gstatic.com")
@@ -15,10 +18,26 @@ func secureHeaders(next http.Handler) http.Handler {
 	})
 }
 
+// Log information on incoming requests
 func (app *application) logRequest(next http.Handler) http.Handler { 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		app.infoLog.Printf("%s - %s %s %s", r.RemoteAddr, r.Proto, r.Method, r.URL.RequestURI())
 		 
 		next.ServeHTTP(w, r)
 	}) 
+}
+
+// Detect panics and send appropriate response to users
+func (app *application) recoverPanic(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// defer this from happening until a panic DOES happen
+		defer func() {
+			if err := recover(); err != nil {
+				w.Header().Set("Connection", "close")
+				app.serverError(w, fmt.Errorf("%s", err))
+			}
+		}()
+
+		next.ServeHTTP(w, r)
+	})
 }
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/windevkay/snippetbox
 go 1.18
 
 require github.com/go-sql-driver/mysql v1.6.0
+
+require github.com/justinas/alice v1.2.0
