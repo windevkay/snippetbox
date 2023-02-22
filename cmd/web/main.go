5f6a4d1c373e6dab493aff925620e11f3aae2f63
kolaadebayo <kolabayo360@gmail.com>
2023-02-22 16:01:59 -0700

    setting up session management

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,7 +7,10 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
+	"github.com/alexedwards/scs/mysqlstore"
+	"github.com/alexedwards/scs/v2"
 	"github.com/go-playground/form/v4"
 	"github.com/windevkay/snippetbox/internal/models"
 
@@ -20,6 +23,7 @@ type application struct {
 	snippets		*models.SnippetModel
 	templateCache 	map[string]*template.Template	
 	formDecoder 	*form.Decoder
+	sessionManager	*scs.SessionManager
 }
 
 func main() {
@@ -47,6 +51,11 @@ func main() {
 
 	formDecoder := form.NewDecoder()
 
+	// session
+	sessionManager := scs.New() 
+	sessionManager.Store = mysqlstore.New(db) 
+	sessionManager.Lifetime = 12 * time.Hour
+
 	// app instance
 	app := &application{
 		errorLog: errorLog,
@@ -54,6 +63,7 @@ func main() {
 		snippets: &models.SnippetModel{DB: db},
 		templateCache: templateCache,
 		formDecoder: formDecoder,
+		sessionManager: sessionManager,
 	}
 
 	// override http defaults e.g. ErrorLog
