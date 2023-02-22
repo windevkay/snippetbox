5e067c2b5c424322a237667a9fb86fca36019e52
kolaadebayo <kolabayo360@gmail.com>
2023-02-22 16:34:52 -0700

    session manager with session data

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -93,6 +93,9 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 		app.serverError(w, err)
 		return
 	}
+
+	app.sessionManager.Put(r.Context(), "flash", "Snippet successfully created!")
+
 	// Redirect the user to the relevant page for the snippet.
 	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
