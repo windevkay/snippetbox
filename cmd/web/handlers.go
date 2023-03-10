e703608e07bcea1b885f5247186b8038fb26325a
kolaadebayo <kolabayo360@gmail.com>
2023-03-09 17:53:31 -0700

    starting login funcs

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -25,6 +25,12 @@ type userSignupForm struct {
 	Password			string `form:"password"`
 	validator.Validator `form:"-"`
 }
+
+type userLoginForm struct {
+	Email					string `form:"email"` 
+	Password				string `form:"password"` 
+	validator.Validator		`form:"-"`
+}
 	
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
@@ -156,7 +162,9 @@ func (app *application) userSignupPost(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) userLogin(w http.ResponseWriter, r *http.Request) { 
-	fmt.Fprintln(w, "Display a HTML form for logging in a user...")
+	data := app.newTemplateData(r)
+	data.Form = userLoginForm{}
+	app.render(w, http.StatusOK, "login.html", data)
 }
 
 func (app *application) userLoginPost(w http.ResponseWriter, r *http.Request) { 
