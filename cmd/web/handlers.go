980489090f68a354b246d300e618afb5352c252a
kolaadebayo <kolabayo360@gmail.com>
2023-03-09 17:23:26 -0700

    user signup logic

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -114,7 +114,45 @@ func (app *application) userSignup(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) userSignupPost(w http.ResponseWriter, r *http.Request) { 
-	fmt.Fprintln(w, "Create a new user...")
+	var form userSignupForm
+	
+	err := app.decodePostForm(r, &form) 
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+	
+	form.CheckField(validator.NotBlank(form.Name), "name", "This field cannot be blank") 
+	form.CheckField(validator.NotBlank(form.Email), "email", "This field cannot be blank") 
+	form.CheckField(validator.Matches(form.Email, validator.EmailRX), "email", "This field must be a valid email address") 
+	form.CheckField(validator.NotBlank(form.Password), "password", "This field cannot be blank") 
+	form.CheckField(validator.MinChars(form.Password, 8), "password", "This field must be at least 8 characters long")
+	// If there are any errors, redisplay the signup form along with a 422 // status code.
+	if !form.Valid() {
+		data := app.newTemplateData(r)
+		data.Form = form
+		app.render(w, http.StatusUnprocessableEntity, "signup.html", data) 
+		return
+	}
+
+	err = app.users.Insert(form.Name, form.Email, form.Password)
+
+	if err != nil {
+		if errors.Is(err, models.ErrDuplicateEmail) { 
+			form.AddFieldError("email", "Email address is already in use")
+			data := app.newTemplateData(r)
+			data.Form = form
+			app.render(w, http.StatusUnprocessableEntity, "signup.html", data)
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
+	// Otherwise add a confirmation flash message to the session confirming that
+	// their signup worked.
+	app.sessionManager.Put(r.Context(), "flash", "Your signup was successful. Please log in.")
+	// And redirect the user to the login page.
+	http.Redirect(w, r, "/user/login", http.StatusSeeOther)
 }
 
 func (app *application) userLogin(w http.ResponseWriter, r *http.Request) { 
--- go.mod
+++ go.mod
@@ -12,4 +12,5 @@ require (
 	github.com/alexedwards/scs/mysqlstore v0.0.0-20230217120314-6b1bedc0f08c // indirect
 	github.com/alexedwards/scs/v2 v2.5.0 // indirect
 	github.com/go-playground/form/v4 v4.2.0 // indirect
+	golang.org/x/crypto v0.7.0 // indirect
 )
