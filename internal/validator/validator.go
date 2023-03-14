a64476d43ef16267f0d85f411c62e1e769f42255
kolaadebayo <kolabayo360@gmail.com>
2023-03-14 15:23:57 -0600

    user login

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -6,7 +6,7 @@ import (
 	"unicode/utf8"
 )
 
-var EmailRX = regexp.MustCompile("/^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/")
+var EmailRX = regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`)
 
 // Define a new Validator type which contains a map of validation errors for our // form fields.
 type Validator struct {
