fa0fabccf214ee2e0af0b8187649335fd1ee4a61
kolaadebayo <kolabayo360@gmail.com>
2023-03-22 14:54:35 -0600

    securing auth routes

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -41,3 +41,17 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 	})
 }
+
+func (app *application) requireAuthentication(next http.Handler) http.Handler { 
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !app.isAuthenticated(r) {
+			http.Redirect(w, r, "/user/login", http.StatusSeeOther)
+			return
+		}
+		// Otherwise set the "Cache-Control: no-store" header so that pages
+		// require authentication are not stored in the users browser cache (or // other intermediary cache).
+		w.Header().Add("Cache-Control", "no-store")
+		// And call the next handler in the chain.
+		next.ServeHTTP(w, r) 
+	})
+}
