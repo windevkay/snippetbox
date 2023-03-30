999e6a2e03e94bab83ccfb00657697e370489d66
kolaadebayo <kolabayo360@gmail.com>
2023-03-29 18:15:06 -0600

    end-to-end test setup

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,38 +1,20 @@
 package main
 
 import (
-	"bytes"
-	"io"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 
 	"github.com/windevkay/snippetbox/internal/assert"
 )
 
 func TestPing(t *testing.T) {
-	// Initialize a new httptest.ResponseRecorder. 
-	rr := httptest.NewRecorder()
-	// Initialize a new dummy http.Request.
-	r, err := http.NewRequest(http.MethodGet, "/", nil) 
-	if err != nil {
-		t.Fatal(err) 
-	}
+	app := newTestApplication(t)
 
-	ping(rr, r)
-
-	rs := rr.Result()
-	// Check that the status code written by the ping handler was 200.
-	assert.Equal(t, rs.StatusCode, http.StatusOK)
-	// And we can check that the response body written by the ping handler // equals "OK".
-	defer rs.Body.Close()
-
-	body, err := io.ReadAll(rs.Body)
-
-	if err != nil { 
-		t.Fatal(err)
-	} 
-
-	bytes.TrimSpace(body)
-	assert.Equal(t, string(body), "OK") 
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+	
+	code, _, body := ts.get(t, "/ping")
+	
+	assert.Equal(t, code, http.StatusOK)
+	assert.Equal(t, body, "OK")
 }
