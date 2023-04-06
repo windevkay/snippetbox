85bb655daef02236f592506acab0ccf4f83cfada
kolaadebayo <kolabayo360@gmail.com>
2023-04-06 14:10:48 -0600

    all done

--- internal/assert/assert.go
+++ internal/assert/assert.go
@@ -15,8 +15,16 @@ func Equal[T comparable](t *testing.T, actual, expected T) {
 
 func StringContains(t *testing.T, actual, expectedSubstring string) {
 	t.Helper()
-	
+
 	if !strings.Contains(actual, expectedSubstring) {
 		t.Errorf("got: %q; expected to contain: %q", actual, expectedSubstring)
+	} 
+}
+
+func NilError(t *testing.T, actual error) { 
+	t.Helper()
+	
+	if actual != nil {
+		t.Errorf("got: %v; expected: nil", actual)
 	} 
 }
